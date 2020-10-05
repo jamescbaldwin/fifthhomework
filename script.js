99d8e318d44156b2bc4bc8408ed42fa7a581f1c3
James Baldwin <jamescbaldwin@gmail.com>
2020-10-05 19:58:05 -0400

    penultimate commit

--- script.js
+++ script.js
@@ -4,50 +4,57 @@ $(document).ready(function () {
     //display current day & time.
     // var newDate = new Date();
     // document.getElementById("dateTime").innerHTML = dateTime;
-    $("#displayDate").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    var dateTime = $("#displayDate").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    console.log('dateTime:', dateTime);
+    // parsedTime = parseInt(moment().hour);
+    // console.log('parsedTime:', parsedTime);
+
+
     //Assign saveBtn click listener for user input and time stamp??
     $(".saveBtn").on("click", function () {
         //get nearby values.
         console.log(this);
-        var text = $(this).siblings(".description").val();
+        var text = $(this).siblings(".content").val();
         var time = $(this).parent().attr("id");
         //set items in local storage.
         localStorage.setItem(time, text);
     })
     //load any saved data from LocalStorage - do this for each hour created.
-    $("#8  .description").val(localStorage.getItem("8"));
-    $("#9  .description").val(localStorage.getItem("9"));
-    $("#10 .description").val(localStorage.getItem("10"));
-    $("#11 .description").val(localStorage.getItem("11"));
-    $("#12 .description").val(localStorage.getItem("12"));
-    $("#13 .description").val(localStorage.getItem("13"));
-    $("#14 .description").val(localStorage.getItem("14"));
-    $("#15 .description").val(localStorage.getItem("15"));
-    $("#16 .description").val(localStorage.getItem("16"));
-    $("#17 .description").val(localStorage.getItem("17"));
+    $("#8  .content").val(localStorage.getItem("8"));
+    $("#9  .content").val(localStorage.getItem("9"));
+    $("#10 .content").val(localStorage.getItem("10"));
+    $("#11 .content").val(localStorage.getItem("11"));
+    $("#12 .content").val(localStorage.getItem("12"));
+    $("#13 .content").val(localStorage.getItem("13"));
+    $("#14 .content").val(localStorage.getItem("14"));
+    $("#15 .content").val(localStorage.getItem("15"));
+    $("#16 .content").val(localStorage.getItem("16"));
+    $("#17 .content").val(localStorage.getItem("17"));
+
     function timetracker() {
         //get current number of hours.
         var currentTime = moment().hour();
         // loop over time blocks
         $(".time-block").each(function () {
-            var idtime = parseInt($(this).attr("id"));
-            console.log( idtime, dateTime)
+            var parsedId = parseInt($(this).attr("id"));
+            console.log(parsedId, currentTime)
+
             //check if we've moved past this time
-            if (idtime < currentTime) {
-                $(this).addClass("past");
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                // $(".description").html("past");
-            }
-            else if (idtime === currentTime) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-                $(this).removeClass("future");
-            }
-            else { //(if idtime > currentTime)
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
+            if (parsedId < currentTime) {
+                $(".content").css('background-color', 'blue');
+                // $(this).addClass("past");
+                // $(this).removeClass("future");
+                // $(this).removeClass("present");
+            } else if (parsedId === currentTime) {
+                $(".content").css('background-color', 'red');
+                // $(this).addClass("present");
+                // $(this).removeClass("past");               
+                // $(this).removeClass("future");
+            } else {
+                $(".content").css('background-color', 'yellow');
+                // $(this).addClass("future");
+                // $(this).removeClass("present");
+                // $(this).removeClass("past");                
             }
         })
     }
